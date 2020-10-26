5c850c3d89288e2bd312d4061335f35491433c6a
Angel Esparza <aesparza@ikkode.com>
2020-10-25 18:58:17 -0600

    docs agregado

---
+++ docs/precache-manifest.8595ef9ce730220392dc1518af3d685c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a8b918abfc6a9aba4d335e8e9effbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0913fa0ababbba9547",
+    "url": "/static/css/main.1793a11c.chunk.css"
+  },
+  {
+    "revision": "d3b4a3e436994039cce0",
+    "url": "/static/js/2.0ff71e30.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0ff71e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0913fa0ababbba9547",
+    "url": "/static/js/main.c7fe0b68.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
